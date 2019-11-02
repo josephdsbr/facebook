3b7fd6cbd12ea5bb76e5ae2486456ce9eaa140d8
josephdsbr <josephdsbr@gmail.com>
2019-11-01 22:13:38 -0300

    adicionando components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import PostList from './Components/PostList';
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+    <Header />
+    <PostList />
+    </>
+  )
 }
 
 export default App;
